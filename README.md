538bea8c82958fce2ad91ece8350207af5c8e437
Marcus Bowman <miliarch.mb@gmail.com>
2019-02-16 21:08:02 -0800

    Swapped getopt for argparse, improved output formatting, updated documentation

--- README.md
+++ README.md
@@ -1,119 +1,119 @@
 # Python Bit Calculator (pybitcalc)
 
-## Synopsis
-    python bitcalc.py [-a amount] [-t type] [-b base]
-
 ## Description
-    A simple python2/3 program for printing a conversion table of different byte values to console.
+    A simple program for printing a conversion table of byte values for a given input value
 
-## Command Line Options
+## Help
 ```
--a float, --amount float
-    Specify amount bits/bytes
+usage: bitcalc.py [-h] [-b BASE] amount type
 
-        
--b int, --base int
-    Specify which base notation to use (only affects -t b and B).
-    See Ubuntu's Units Policy for more info:
-    https://wiki.ubuntu.com/UnitsPolicy
-        
-    Valid Options
-    2 - 1024 bytes in kilobyte/base-2/IEC Notation (default)
-    10 - 1000 bytes in kilobyte/base-10/SI Notation
+A simple program for printing a conversion table of byte values for a given input value
 
--h, --help
-    Print usage information
+positional arguments:
+  amount                specify bit/byte amount
+  type                  specify bit/byte type as short unit label - valid options:
+                          ambiguous: [b|B] (handled as base-2 by default)
+                          base-2: [Kib|KiB|Mib|MiB|Gib|GiB|Tib|TiB|Pib|PiB]
+                          base-10: [kb|kB|Mb|MB|Gb|GB|Tb|TB|Pb|PB]
 
-        
--t string, --type string
-    Specify type of amount
-        
-    Valid Options
-    Flexible - b, B (default base-2)
-    base-2 - Kib, KiB, Mib, MiB, Gib, GiB, Tib, TiB, Pib, PiB
-    base-10 - kb, kB, Mb, MB, Gb, GB, Tb, TB, Pb, PB
+optional arguments:
+  -h, --help            show this help message and exit
+  -b BASE, --base BASE  specify base for ambiguous unit labels - valid options: [2|10]
 ```
+
+For more information about IEC and SI notation, see [Ubuntu Units Policy](https://wiki.ubuntu.com/UnitsPolicy).
 
 ## Usage and Output Examples
-**$ ./bitcalc.py -a 1024 -t B**
+**$ ./bitcalc.py 1024 B**
 ```
 Bytes selected, no base specified, using base-2.
-Run again and specify -b 10 or --base 10 for base-10.
+Run again with -b 10 or --base 10 for base-10.
 
-Unit Type           Value
-  b - Bits          8192
-  B - Bytes         1024
-Kib - Kibibits      8
-KiB - Kibibytes     1
-Mib - Mebibits      0.0078125
-MiB - Mebibytes     0.0009765625
-Gib - Gibibits      0.000007629394531
-GiB - Gibibytes     0.000000953674316
-Tib - Tebibits      0.000000007450581
-TiB - Tebibytes     0.000000000931323
-Pib - Pebibits      0.000000000007276
-PiB - Pebibytes     0.000000000000909
-```
-
-**$ ./bitcalc.py -a 1000 -t B -b 10**
-```
-Unit Type           Value
- b - Bits           8000
- B - Bytes          1000
-kb - Kilobits       8
-kB - Kilobytes      1
-Mb - Megabits       0.008
-MB - Megabytes      0.001
-Gb - Gigabits       0.000008
-GB - Gigabytes      0.000001
-Tb - Terabits       0.000000008
-TB - Terabytes      0.000000001
-Pb - Petabits       0.000000000008
-PB - Petabytes      0.000000000001
+Input value: 1024.0 B
++-------------------+------------------------+
+| (lbl) Unit Label  |         Value (base-2) |
++-------------------+------------------------+
+|   (b) Bits        |                   8192 |
+|   (B) Bytes       |                   1024 |
+| (Kib) Kibibits    |                      8 |
+| (KiB) Kibibytes   |                      1 |
+| (Mib) Mebibits    |              0.0078125 |
+| (MiB) Mebibytes   |           0.0009765625 |
+| (Gib) Gibibits    |      0.000007629394531 |
+| (GiB) Gibibytes   |      0.000000953674316 |
+| (Tib) Tebibits    |      0.000000007450581 |
+| (TiB) Tebibytes   |      0.000000000931323 |
+| (Pib) Pebibits    |      0.000000000007276 |
+| (PiB) Pebibytes   |      0.000000000000909 |
++-------------------+------------------------+
 ```
 
-**$ ./bitcalc.py -a 53.7 -t GiB**
+**$ ./bitcalc.py 1000 B -b 10**
 ```
-Unit Type           Value
-  b - Bits          461279487590.4
-  B - Bytes         57659935948.8
-Kib - Kibibits      450468249.6
-KiB - Kibibytes     56308531.2
-Mib - Mebibits      439910.4
-MiB - Mebibytes     54988.8
-Gib - Gibibits      429.6
-GiB - Gibibytes     53.7
-Tib - Tebibits      0.41953125
-TiB - Tebibytes     0.05244140625
-Pib - Pebibits      0.000409698486328
-PiB - Pebibytes     0.000051212310791
+Input value: 1000.0 B
++-------------------+------------------------+
+| (lbl) Unit Label  |        Value (base-10) |
++-------------------+------------------------+
+|   (b) Bits        |                   8000 |
+|   (B) Bytes       |                   1000 |
+|  (kb) Kilobits    |                      8 |
+|  (kB) Kilobytes   |                      1 |
+|  (Mb) Megabits    |                  0.008 |
+|  (MB) Megabytes   |                  0.001 |
+|  (Gb) Gigabits    |               0.000008 |
+|  (GB) Gigabytes   |               0.000001 |
+|  (Tb) Terabits    |            0.000000008 |
+|  (TB) Terabytes   |            0.000000001 |
+|  (Pb) Petabits    |         0.000000000008 |
+|  (PB) Petabytes   |         0.000000000001 |
++-------------------+------------------------+
 ```
 
-**$ ./bitcalc.py -a 53.7 -t GB**
+**$ ./bitcalc.py 53.7 GiB**
 ```
-Unit Type           Value
- b - Bits           429600000000
- B - Bytes          53700000000
-kb - Kilobits       429600000
-kB - Kilobytes      53700000
-Mb - Megabits       429600
-MB - Megabytes      53700
-Gb - Gigabits       429.6
-GB - Gigabytes      53.7
-Tb - Terabits       0.4296
-TB - Terabytes      0.0537
-Pb - Petabits       0.0004296
-PB - Petabytes      0.0000537
+Input value: 53.7 GiB
++-------------------+------------------------+
+| (lbl) Unit Label  |         Value (base-2) |
++-------------------+------------------------+
+|   (b) Bits        |         461279487590.4 |
+|   (B) Bytes       |          57659935948.8 |
+| (Kib) Kibibits    |            450468249.6 |
+| (KiB) Kibibytes   |             56308531.2 |
+| (Mib) Mebibits    |               439910.4 |
+| (MiB) Mebibytes   |                54988.8 |
+| (Gib) Gibibits    |                  429.6 |
+| (GiB) Gibibytes   |                   53.7 |
+| (Tib) Tebibits    |             0.41953125 |
+| (TiB) Tebibytes   |          0.05244140625 |
+| (Pib) Pebibits    |      0.000409698486328 |
+| (PiB) Pebibytes   |      0.000051212310791 |
++-------------------+------------------------+
+```
+
+**$ ./bitcalc.py 53.7 GB**
+```
+Input value: 53.7 GB
++-------------------+------------------------+
+| (lbl) Unit Label  |        Value (base-10) |
++-------------------+------------------------+
+|   (b) Bits        |           429600000000 |
+|   (B) Bytes       |            53700000000 |
+|  (kb) Kilobits    |              429600000 |
+|  (kB) Kilobytes   |               53700000 |
+|  (Mb) Megabits    |                 429600 |
+|  (MB) Megabytes   |                  53700 |
+|  (Gb) Gigabits    |                  429.6 |
+|  (GB) Gigabytes   |                   53.7 |
+|  (Tb) Terabits    |                 0.4296 |
+|  (TB) Terabytes   |                 0.0537 |
+|  (Pb) Petabits    |              0.0004296 |
+|  (PB) Petabytes   |              0.0000537 |
++-------------------+------------------------+
 ```
 
 ## Version History / Change Log
+02/16/2019 - v1.1 - Refactoring and general improvements
 09/14/2016 - v1.0 - First functional release
 
 ## About
-This is a little project that's been in the works for a while. Frequently dealing with storage/filesystem management, I found that I'd often need to convert between different bit and byte values. Most of the time, I'd just head over to [Matisse's Bit Calculator](http://www.matisse.net/bitcalc/), as it suited my needs perfectly. Over time, this became a bit cumbersome for my needs (connect to internet, launch browser, visit site), and I sought to find a locally executable equivalent. **I didn't find one!**
-
-I originally built this bit calculator as a Java GUI program. This was great, until I:
-* Developed a preference for command line applications
-* Discovered how much of a blast Python programming can be
-
-I don't currently have plans to release the Java code, but I might just revisit it in the future.
+This is a little project that's been in the works for a while. Frequently dealing with storage/filesystem management, I found that I'd often need to convert between different bit and byte values. Most of the time, I'd just head over to [Matisse's Bit Calculator](http://www.matisse.net/bitcalc/), as it suited my needs perfectly. Over time, this became a bit cumbersome for my needs (connect to internet, launch browser, visit site), and I sought to find a locally executable equivalent. **I didn't find one, so I built one!**
--- bitcalc.py
+++ bitcalc.py
@@ -3,11 +3,11 @@
 Inspired by Matisse's Bit Calculator - http://www.matisse.net/bitcalc
 Units conform to Ubuntu Units Policy - https://wiki.ubuntu.com/UnitsPolicy
 
-License: MIT License, Copyright (c) 2016 Marcus Bowman
-Version: 1.0
+License: MIT License, Copyright (c) 2016-2019 Marcus Bowman
+Version: 1.1
 '''
 import sys
-from getopt import getopt, GetoptError
+from argparse import ArgumentParser, RawTextHelpFormatter
 
 # Set IEC (base-2) and SI (base-10) values - should be considered constant
 b2 = 1024
@@ -24,7 +24,7 @@ b2_units = [
     'gibibit', 'gibibyte',
     'tebibit', 'tebibyte',
     'pebibit', 'pebibyte',
-    ]
+]
 
 # List of short base_2 unit labels
 b2_units_s = [
@@ -34,7 +34,7 @@ b2_units_s = [
     'Gib', 'GiB',
     'Tib', 'TiB',
     'Pib', 'PiB',
-    ]
+]
 
 # List of base-10 unit labels
 b10_units = [
@@ -44,7 +44,7 @@ b10_units = [
     'gigabit', 'gigabyte',
     'terabit', 'terabyte',
     'petabit', 'petabyte',
-    ]
+]
 
 # List of short base_10 unit labels
 b10_units_s = [
@@ -54,15 +54,15 @@ b10_units_s = [
     'Gb', 'GB',
     'Tb', 'TB',
     'Pb', 'PB',
-    ]
+]
 
 # Create and populate dictionary for short to long unit mapping (all bases)
 unit_table = {}
 
-for idx,unit in enumerate(b2_units):
+for idx, unit in enumerate(b2_units):
     unit_table[b2_units_s[idx]] = unit
 
-for idx,unit in enumerate(b10_units):
+for idx, unit in enumerate(b10_units):
     unit_table[b10_units_s[idx]] = unit
 
 # Modify Similar - Number of bytes in kilobyte, b2 (1024) by default
@@ -70,6 +70,7 @@ mod_s = b2
 
 # Modify Disparate - Number of bits in byte
 mod_d = 8
+
 
 def convert_to_bits(input_type, input_value):
     """ Import type and value of input, return value in bits """
@@ -80,7 +81,7 @@ def convert_to_bits(input_type, input_value):
     if not is_bit:
         input_value *= mod_d
 
-    # Check prefix and convert to bits accordingly    
+    # Check prefix and convert to bits accordingly
     for prefix in prefixes:
         # Do nothing unless prefix is found in input_type string
         if prefix in input_type[:2]:
@@ -103,107 +104,101 @@ def convert_to_bits(input_type, input_value):
     return input_value
 
 
-def get_args():
-    """ Import command line arguments, return dictionary of passed args """
-    argv = sys.argv[1:]
+def format_divider(scan_str, match_char='|', i_char='+', fill_char='-'):
+    """ Format and return a divider for use in table printing - check the
+    scan_str for all instances of the match_char, log the positions, generate
+    divider with i_char in match positions, fill_char in non-match positions
+    """
+    i_positions = [idx for idx, s in enumerate(scan_str) if s == match_char]
+    divider = ''
+    for i in range(len(scan_str)):
+        if i in i_positions:
+            divider += i_char
+        else:
+            divider += fill_char
+    return divider
 
-    try:
-        opts, args = getopt(argv, "a:b:ht:", ["amount=", "base=", "help", "type="])
-    except GetoptError:
-        usage()
 
-    arg_dict = {}
+def format_output(lbl, label, value):
+    return "| {lbl: >5} {label: <12}|{value: >23} |".format(
+        lbl='({})'.format(lbl),
+        label=label,
+        value=value)
 
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            usage()
+
+def parse_args():
+    """ Parse passed arguments and return as Namespace object """
+    # Found this bit posted by unutbu here: http://stackoverflow.com/a/4042861
+    class Parser(ArgumentParser):
+        def error(self, message):
+            sys.stderr.write('error: {0}\n'.format(message))
+            self.print_help()
             sys.exit(2)
-        elif opt in ("-a", "--amount"):
-            try:
-                arg_dict['value'] = float(arg)
-            except(TypeError, ValueError):
-                print("Amount (-a, --amount) must be a number, try again.")
-                usage()
-        elif opt in ("-b", "--base"):
-            try:
-                arg_dict['base'] = int(arg)
-            except(TypeError, ValueError):
-                print("Base (-b, --base) must be a number, try again.")
-                usage()
-        elif opt in ("-t", "--type"):
-            arg_dict['type'] = arg
 
-    return arg_dict
+    # Set and define description
+    desc = 'A simple program for printing a conversion '
+    desc += 'table of byte values for a given input value'
+    parser = Parser(description=desc, formatter_class=RawTextHelpFormatter)
+
+    help_str = 'specify bit/byte amount'
+    parser.add_argument('amount', help=help_str)
+
+    help_str = 'specify bit/byte type as short unit label - valid options:'
+    help_str += '\n  ambiguous: [{}] (handled as base-2 by default)'.format(
+        '|'.join(b2_units_s[:2]))
+    help_str += '\n  base-2: [{}]'.format('|'.join(b2_units_s[2:]))
+    help_str += '\n  base-10: [{}]'.format('|'.join(b10_units_s[2:]))
+    help_str += '\n'
+    parser.add_argument('type', help=help_str)
+
+    help_str = 'specify base for ambiguous unit labels - valid options: [2|10]'
+    help_str += ''
+    parser.add_argument('-b', '--base', help=help_str)
+
+    return parser.parse_args()
 
 
 def print_table(bit_value):
     """ Print conversion table based on bit_value to console """
     table_values = []
-    base_2 = True if mod_s == b2 else False
-
-    # Define and print header string
-    header = "Unit Type\t\tValue"
+    if mod_s == b2:
+        base = 'base-2'
+    else:
+        base = 'base-10'
+    header = format_output('lbl', 'Unit Label', 'Value ({})'.format(base))
+    divider = format_divider(header)
+    print(divider)
     print(header)
+    print(divider)
 
-    for idx,unit in enumerate(units):
+    for idx, unit in enumerate(units):
         if idx == 0:
             # Append base bit value to table_values list
-            table_values.append(bit_value);
+            table_values.append(bit_value)
         elif idx == 1:
             # Append base byte value to table_values list
             table_values.append(bit_value / mod_d)
         else:
             # Modify value for previous like type (bit/byte), append to list
             table_values.append(table_values[idx - 2] / mod_s)
-        
+
         if table_values[idx] > 1:
             # Format float as 3 point decimal for larger amounts
-            pretty_val = "{:0.3f}".format(table_values[idx]).rstrip("0").rstrip(".")
+            pretty_val = "{:0.3f}".format(
+                table_values[idx]).rstrip("0").rstrip(".")
         else:
             # Format float as 15 point decimal for smaller amounts
-            pretty_val = "{:0.15f}".format(table_values[idx]).rstrip("0").rstrip(".")
-        
+            pretty_val = "{:0.15f}".format(
+                table_values[idx]).rstrip("0").rstrip(".")
+
         # Set unit labels for long and short labels
         unit_long = unit.title()
-
-        if len(units_s[idx]) == 1:
-            # Adjust short unit label alignment if b or B (insert spaces)
-            unit_short = "  " + units_s[idx] if base_2 else " " + units_s[idx]
-        else:
-            unit_short = units_s[idx]
+        unit_short = units_s[idx]
 
         # Define and print output string
-        output_str = ("{short} - {unit}s\t\t{val}".format(
-                unit=unit_long, short=unit_short, val=pretty_val
-                )
-            )
+        output_str = format_output(unit_short, unit_long + 's', pretty_val)
         print(output_str)
-
-
-def usage():
-    """ Print usage information to console """
-    usage_str = (
-        "SYNOPSIS\n\tpython bitcalc.py [-a amount] [-t type] [-b base]\n\n"
-        "DESCRIPTION\n\tA simple python2/3 program for printing a conversion "
-        "table of different byte values to console.\n\n"
-        "COMMAND LINE OPTIONS\n\t-a float, --amount float\n\t\tSpecify amount "
-        "bits/bytes\n\n\t-b int, --base int\n\t\tSpecify which base notation "
-        "to use (only affects -t b and B).\n\n\t\tVALID OPTIONS\n\t\t2 - 1024 "
-        "bytes in kilobyte/base-2/IEC Notation (default)\n\t\t10 - 1000 bytes"
-        "in kilobyte/base-10/SI Notation\n\n\t-h, --help\n\t\tPrint usage "
-        "information (this menu)\n\n\t-t string, --type string\n\t\tSpecify "
-        "type of amount\n\n\t\tVALID OPTIONS\n\t\tFlexible - b, B (default "
-        "base-2)\n\t\tbase-2 - Kib, KiB, Mib, MiB, Gib, GiB, Tib, TiB, Pib, "
-        "PiB\n\t\tbase-10 - kb, kB, Mb, MB, Gb, GB, Tb, TB, Pb, PB\n\nEXAMPLES"
-        "\n\t$ ./bitcalc.py -a 1024 -t B\n\t\tReturns conversion table of "
-        "1024 Bytes (base-2)\n\n\t$ ./bitcalc.py -a 1000 -t B -b 10\n\t\t"
-        "Returns conversion table of 1000 Bytes (base-10)\n\n\t$ ./bitcalc.py "
-        "-a 53.7 -t GiB\n\t\tReturns conversion table of 53.7 Gibibytes "
-        "(base-2)\n\n\t$ ./bitcalc.py -a 53.7 -t GB\n\t\tReturns conversion "
-        "table of 53.7 Gigabytes (base-10)"
-        )
-    print(usage_str)
-    sys.exit(2)
+    print(divider)
 
 
 def update_mod_s(value):
@@ -211,56 +206,64 @@ def update_mod_s(value):
     global mod_s
     mod_s = value
 
-def validate_args(arg_dict):
-    # Check if arguments have been passed
-    type_exists = True if 'type' in arg_dict else False
-    base_exists = True if 'base' in arg_dict else False
 
-    # Check if type passed as arg, continue if true
-    if type_exists:
-        # Validate specified type exists in the unit_table
-        if arg_dict['type'] not in unit_table:
-            print("Invalid type {0}, check usage.".format(arg_dict['type']))
-            usage()
-    else:
-        # TODO: Establish default type if no type present
-        # Provide usage if type is not present
-        print("No type provided, check usage.")
-        usage()
+def validate_args(args):
+    # Cast amount and base (if present) as integer
+    try:
+        args.amount = float(args.amount)
+    except ValueError:
+        help_str = 'Amount must be a number'
+        print(help_str)
+        sys.exit(2)
+
+    if args.base:
+        try:
+            args.base = int(args.base)
+        except ValueError:
+            help_str = 'Base must be a number'
+            print(help_str)
+            sys.exit(2)
+
+    # Validate specified type exists in the unit_table
+    if args.type not in unit_table:
+        help_str = "Invalid type: {0}".format(args.type)
+        print(help_str)
+        sys.exit(2)
 
     # Set input_type for simplified use in later logic
-    input_type = unit_table[arg_dict['type']]
+    input_type = unit_table[args.type]
 
     # Check if base passed as arg, continue only if true and type is bit/byte
-    if base_exists and (input_type == 'bit' or input_type == 'byte'):
+    if args.base and (input_type == 'bit' or input_type == 'byte'):
         # Validate base specification, only 2 and 10 are valid
-        if arg_dict['base'] != 2 and arg_dict['base'] != 10:
-            print('Base specification must be 2 or 10, check usage.')
-            usage()
+        if args.base != 2 and args.base != 10:
+            usage_str = 'Base specification must be 2 or 10'
+            print(usage_str)
+            sys.exit(2)
         # Update mod_s to binary/base-2 if specified
-        elif arg_dict['base'] == 2:
+        elif args.base == 2:
             update_mod_s(b2)
         # Update mod_s to decimal/base-10 if specified
-        elif arg_dict['base'] == 10:
+        elif args.base == 10:
             update_mod_s(b10)
     else:
-        # Check input_type, if bit/byte, notify user binary/base-2 will be used 
+        # Check input_type, if bit/byte, notify user binary/base-2 will be used
         if input_type == 'bit' or input_type == 'byte':
             usage_str = ('{0}s selected, no base specified, using base-2.'
-                '\nRun again and specify -b 10 or --base 10 for base-10.\n'
-                ''.format(input_type.title()))
+                         '\nRun again with -b 10 or --base 10 for base-10.\n'
+                         ''.format(input_type.title()))
             print(usage_str)
         # Check input_type, update mod_s to decimal/base-10 if not "bi" type
         elif 'bib' not in input_type:
             update_mod_s(b10)
 
+    return args
+
 
 def main():
-    # Import arguments
-    arg_dict = get_args()
-
-    # Validate arguments
-    validate_args(arg_dict)
+    # Import and validate arguments
+    args = parse_args()
+    args = validate_args(args)
 
     # Create global vars to hold selected units
     global units, units_s
@@ -269,12 +272,14 @@ def main():
     units = b2_units if mod_s == b2 else b10_units
     units_s = b2_units_s if mod_s == b2 else b10_units_s
 
-    # Store arguments for further use
-    input_value = arg_dict['value']
-    input_type = unit_table[arg_dict['type']]
-
     # Convert input value to bits
-    bit_value = convert_to_bits(input_type, input_value)
+    bit_value = convert_to_bits(unit_table[args.type], args.amount)
+
+    # Print input value
+    input_value_str = 'Input value: {value} {lbl}'.format(
+        value=args.amount,
+        lbl=args.type)
+    print(input_value_str)
 
     # Print conversion table
     print_table(bit_value)
