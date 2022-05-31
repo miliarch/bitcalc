277753ffabefa63ab305daa702ae1551929d0501
Marcus Bowman <miliarch.mb@gmail.com>
2022-05-30 21:28:48 -0700

    Update to support packaging and listing on PyPi

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+BASE_DIR = Path(__file__).parent
+long_description = (BASE_DIR / "README.md").read_text()
 setup(
     name="bitcalc",
-    version="1.4",
+    version="1.5",
     packages=find_packages(),
     python_requires='>=3',
     install_requires=[],
     author='Marcus Bowman',
     author_email='miliarch.mb@gmail.com',
     description='A python module for managing bit/byte unit manipulation',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license='MIT',
     keywords='bit byte bitcalc binary decimal base-2 base-10 data rate transfer time',
     url='https://github.com/miliarch/bitcalc',
